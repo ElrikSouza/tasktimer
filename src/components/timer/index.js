7a4ef2e28d5aef762771fcea7e2e75d1d4f23bf0
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-06-02 21:56:58 -0400

    created timer component

---
+++ src/components/timer/index.js
@@ -0,0 +1,25 @@
+import "./timer.css";
+
+const padNumberWithZeros = (number, numZeros = 2) => {
+  return number.toString().padStart(numZeros, 0);
+};
+
+const secondsToTimeString = (seconds) => {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds - hours * 3600) / 60);
+  const remainingSeconds = seconds - hours * 3600 - minutes * 60;
+
+  return `${padNumberWithZeros(hours)}:${padNumberWithZeros(
+    minutes
+  )}:${padNumberWithZeros(remainingSeconds)}`;
+};
+
+export default function Timer({ currentTimeInSeconds }) {
+  return (
+    <div className="timer">
+      <div className="timer_value">
+        {secondsToTimeString(currentTimeInSeconds)}
+      </div>
+    </div>
+  );
+}

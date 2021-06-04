7087d1ffc42865414a3c568417ac11216abb0c8e
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-06-04 12:02:41 -0400

    updated App.js to use hooks

--- src/App.js
+++ src/App.js
@@ -1,102 +1,68 @@
-import { useEffect, useState } from "react";
 import TaskList from "./components/tasklist";
-import "./App.css";
 import TimerView from "./components/timerview";
-import { loadTasks, saveTasks } from "./utils/tasks";
 import { usePageWidth } from "./components/hooks/use-pagewidth";
 import TaskListContainier from "./components/tasklist/container";
-import { loadTimer, saveTimer } from "./utils/timer";
-import { useBeforeUnload } from "./components/hooks/usebeforeunload";
+import { useTimer } from "./components/hooks/use-timer";
+import { useFormField } from "./components/hooks/use-form-field";
+import { useTasks } from "./components/hooks/use-tasks";
+import "./App.css";
 
 function App() {
-  const [currentTimeInSeconds, setCurrentTimeInSeconds] = useState(loadTimer());
-  const [isTimerActive, setTimerActive] = useState(true);
-  const [taskArray, setTaskArray] = useState(loadTasks());
-  const [taskName, setTaskName] = useState("");
+  const { tasks, addNewTask, removeFirstTask, removeTaskById } = useTasks();
+
+  const {
+    value: taskName,
+    onChange: onChangeTaskname,
+    clear: clearTaskName,
+  } = useFormField();
+
   const { width } = usePageWidth();
 
-  useEffect(() => {
-    const timer = setTimeout(
-      () =>
-        isTimerActive &&
-        currentTimeInSeconds > 0 &&
-        taskArray.length > 0 &&
-        setCurrentTimeInSeconds(currentTimeInSeconds - 1),
-      1000
-    );
+  const getNumberOfTasks = () => tasks.length;
 
-    return () => clearTimeout(timer);
-  });
-
-  const saveCurrentTime = () => saveTimer(currentTimeInSeconds);
-  useBeforeUnload(saveCurrentTime);
-
-  const maxId = () => {
-    if (taskArray.length === 0) {
-      return 0;
-    }
-
-    return taskArray[taskArray.length - 1].id;
-  };
+  const { timeInSeconds, isTimerPaused, onPauseResume, resetTimer } =
+    useTimer(getNumberOfTasks);
 
   const onSubmit = () => {
-    if (taskArray.length === 0) {
-      setCurrentTimeInSeconds(1800);
+    if (tasks.length === 0) {
+      resetTimer();
     }
 
-    const newTaskArray = [
-      ...taskArray,
-      { name: taskName, id: maxId(taskArray) + 1 },
-    ];
-
-    setTaskArray(newTaskArray);
-    setTaskName("");
-    saveTasks(newTaskArray);
-  };
-
-  const onChange = (event) => {
-    setTaskName(event.target.value);
-  };
-
-  const onPauseResume = () => {
-    setTimerActive(!isTimerActive);
+    addNewTask(taskName);
+    clearTaskName();
   };
 
   const popAndMoveOn = () => {
-    const [, ...rest] = taskArray;
-    setTaskArray(rest);
-    setCurrentTimeInSeconds(1800);
-    saveTasks(rest);
+    removeFirstTask();
+    resetTimer();
   };
 
   const onDeleteTask = (taskId) => {
-    if (taskArray[0].id === taskId) {
-      setCurrentTimeInSeconds(1800);
+    if (tasks[0].id === taskId) {
+      resetTimer();
     }
 
-    const newTaskArray = taskArray.filter((task) => task.id !== taskId);
-    setTaskArray(newTaskArray);
-    saveTasks(newTaskArray);
+    removeTaskById(taskId);
   };
 
   return (
     <div className="App">
       <TimerView
-        currentTimeInSeconds={currentTimeInSeconds}
-        isTaskCompleted={currentTimeInSeconds === 0}
-        areThereAnyTasks={taskArray.length > 0}
+        currentTimeInSeconds={timeInSeconds}
+        isTaskCompleted={timeInSeconds === 0}
+        areThereAnyTasks={tasks.length > 0}
         onConfirmCompleteTask={popAndMoveOn}
         onPauseResume={onPauseResume}
-        isTimerPaused={!isTimerActive}
-        currentTask={taskArray[0] == null ? null : taskArray[0].name}
+        isTimerPaused={isTimerPaused}
+        currentTask={tasks[0] == null ? null : tasks[0].name}
       />
 
       <TaskListContainier width={width}>
         <TaskList
-          tasks={taskArray}
+          tasks={tasks}
           onSubmit={onSubmit}
           addTaskValue={taskName}
-          onChangeAddTaskValue={onChange}
+          onChangeAddTaskValue={onChangeTaskname}
           onDeleteTask={onDeleteTask}
         />
       </TaskListContainier>

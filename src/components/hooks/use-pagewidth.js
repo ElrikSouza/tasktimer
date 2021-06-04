0993471eb33d39e2050f7f5c8630777308154d90
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-06-04 11:12:22 -0400

    created usePageWidth hook

---
+++ src/components/hooks/use-pagewidth.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+export const usePageWidth = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const handleWindowResize = () => setWidth(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+    return () => window.removeEventListener("resize", handleWindowResize);
+  });
+
+  return { width };
+};
